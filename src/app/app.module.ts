c6dd9c395b1ff2f000db3bc335407f4a2cd36768
Y <i_try_it@rambler.ru>
2018-07-27 23:07:34 +0300

    describe module parts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,42 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
+    /**
+     * list of components, directives, and pipes that are a part of the specific
+     * module
+     * Everything in a declaration is visible to application using this module,
+     * without requiring any explicit exports
+     */
     AppComponent
   ],
   imports: [
+    /**
+     *  HERE WE IMPORT MODULERS
+     * you can make other modules visible within this module
+     * if another developer imports your module, they may not be able to see the
+     * modules you have imported
+     */
     BrowserModule
   ],
-  providers: [],
+  exports: [
+    /**
+     * the list or any declarations that you want to be made available (visible)
+     * to other modules that may import this module.
+     */
+  ],
+  providers: [
+    /**
+     * IMPORS SERVICE, STATIC OBJECT, SIMPLE VALUES, CLASSES
+     * used to list providers for configuring the injector, when this module is
+     * imported by other modules
+     */
+  ],
+  /**
+   * entryComponents - constitutes a list of components about which
+   * ComponentFactory should be generated. Primarily used for dynamically
+   * loading components.
+   */
+   // TELS TO MOUNT THIS COMPONENT IMMEDIATLY ON APP START
   bootstrap: [AppComponent]
 })
 export class AppModule { }
